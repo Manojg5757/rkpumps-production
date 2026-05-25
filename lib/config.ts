586db74adba376cb2d7cfcacb5fb431540a23c7e
Manojg5757 <gmanojece2021@gmail.com>
2026-05-25 20:18:23 +0400

    pincode change

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,7 @@
 export const BUSINESS_INFO = {
   name: "R.K Pumps & Motors",
   address1: "Vadambacheri",
-  address2: "Coimbatore,641671",
+  address2: "Coimbatore,641669",
   phone: "9688265022",
   gstin: "",
   email: "saravanaofficial5@gmail.com",
